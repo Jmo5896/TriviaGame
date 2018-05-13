8ad94fa0b04c1723ad6539c01c44c6109378ce39
Jmo5896 <jmo5896@moore-clan.net>
2018-05-12 19:24:18 -0600

    polished commit

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -7,113 +7,138 @@
 
 // Don't let the player pick more than one answer per question.
 // Don't forget to include a countdown timer.
+$('#start').on('click', function(){
+    $('#start').remove();
+    game.loadQuestion();
+});
 
-var timerID;
+$(document).on('click', '.answer-button', function(e) {
+    game.clicked(e);
+});
 
-var count = 0;
+$(document).on('click', '#reset', function() {
+    game.reset();
+});
+
 var questions = [
     {
         question: 'question 1?',
         choices: [
             'a', 'b', 'c', 'd'
         ],
-        answerIndex: 1
+        correctAnswer: 'a'
     },
     {
         question: 'question 2?',
         choices: [
             '1', '2', '3', '4'
         ],
-        answerIndex: 2
+        correctAnswer: '3'
     },
     {
         question: 'question 3?',
         choices: [
             'a', 'b', 'c', 'd'
         ],
-        answerIndex: 0
+        correctAnswer: 'b'
     }
 ];
-var currentQuestion;
-var currentChoices;
-//start button
-$('.startButton').on('click', startGame);
+ 
+//need an object to represent game
+    //inside object we need: counter, answeredCorrectly, answered Incorrectly,
+    // unAnswered, question
+        //methods: countdown, load question, next question, answered correctly, answered 
+        // incorrectly, times up, clicked, results, reset
+var timer;
+var game = {
+    counter: 30,
+    correct: 0,
+    incorrect: 0,
+    unAnswered: 0,
+    currentQuestion: 0,
+    question: questions,
+    countDown: function() {
+        game.counter--;
+        $('#counter').html(game.counter);
+        if (game.counter <= 0) { 
+            console.log('times up'); 
+            game.timeUp();
+        }
+          
+    },
+    loadQuestion: function() {
+        timer = setInterval(game.countDown, 1000);
+        $('.gameGoesHere').html('<h2>TIME REMAINING: <span id = "counter">30</span> Seconds</h2>');
+        $('.gameGoesHere').append('<h2>' + questions[game.currentQuestion].question + '</h2>');
 
-function startGame() {
-    //TODO: loop thru array of questions 
-    timerID = setInterval(nextQuestion, 1000*11);
-        //call showQuestion function and pass it the questions and choices from the current 
-        //object that is being iterrated
-            
-    //TODO: start the timer and assign it to timerID
-    //stack overflow, it works
-    function startTimer(duration, display) {
-        var timer = duration, minutes, seconds;
-        setInterval(function () {
-            seconds = parseInt(timer % 60, 10);
-    
-            seconds = seconds < 10 ? "0" + seconds : seconds;
-    
-            display.text(seconds);
-    
-            if (--timer < 0) {
-                timer = duration;
-            }
-        }, 1000);
+        for (var i = 0; i < questions[game.currentQuestion].choices.length; i++) {
+            $('.gameGoesHere').append('<button class= "answer-button" id = "button-' + i + '" data-name = "' + questions[game.currentQuestion].choices[i] + '">' + questions[game.currentQuestion].choices[i] + '</button>');
+        }
+    },
+    nextQuestion: function() {
+        clearInterval(timer);
+        game.counter = 30;
+        $('#counter').html(game.counter);
+        game.currentQuestion++;
+        game.loadQuestion();
+    }, 
+    answeredCorrectly: function() {
+        clearInterval(timer);
+        game.correct++;
+        $('.gameGoesHere').html('<h2>YOU GOT IT RIGHT!!!</h2>');
+        if (game.currentQuestion == questions.length - 1) {
+            setTimeout(game.results, 3 * 1000);
+        } else {
+            setTimeout(game.nextQuestion, 3 * 1000);
+        }
+    },
+    answeredIncorrectly: function() {
+        clearInterval(timer);
+        game.incorrect++;
+        $('.gameGoesHere').html('<h2>YOU GOT IT WRONG!!!</h2>');
+        $('.gameGoesHere').append('<h3>The Correct Answer Was: ' + questions[game.currentQuestion].correctAnswer + '</h3>');
+        if (game.currentQuestion == questions.length - 1) {
+            setTimeout(game.results, 3 * 1000);
+        } else {
+            setTimeout(game.nextQuestion, 3 * 1000);
+        }
+    },
+    timeUp: function() {
+        clearInterval(timer);
+        game.unAnswered++;
+        $('.gameGoesHere').html('<h2>OUT OF TIME!</h2>');
+        $('.gameGoesHere').append('<h3>The Correct Answer Was: ' + questions[game.currentQuestion].correctAnswer + '</h3>');
+        if (game.currentQuestion == questions.length - 1) {
+            setTimeout(game.results, 3 * 1000);
+        } else {
+            setTimeout(game.nextQuestion, 3 * 1000);
+        }
+    },
+    clicked: function(e) {
+        clearInterval(timer);
+        if ($(e.target).data('name') == questions[game.currentQuestion].correctAnswer) {
+            game.answeredCorrectly();
+        } else {
+            game.answeredIncorrectly();
+        }
+    },
+    results: function() {
+        clearInterval(timer);
+        $('.gameGoesHere').html('<h2>ALL DONE!</h2>');
+        $('.gameGoesHere').append('<h3>Correct: ' + game.correct + '</h3>');
+        $('.gameGoesHere').append('<h3>Incorrect: ' + game.incorrect + '</h3>');
+        $('.gameGoesHere').append('<h3>unanswered: ' + game.unAnswered + '</h3>');
+        $('.gameGoesHere').append('<button id = "reset">RESET</button>');
+    }, 
+    reset: function() {
+        clearInterval(timer);
+        game.currentQuestion = 0;
+        game.counter = 30;
+        game.correct = 0;
+        game.incorrect = 0;
+        game.unAnswered = 0;
+        game.loadQuestion();
     }
-    
-    jQuery(function ($) {
-        var thirtySeconds = 10,
-            display = $('.displayTimer');
-        startTimer(thirtySeconds, display);
-    });
-    
 }
 
-function showQuestion(question, choices) {
-    //TODO: use jquery to create a question div, a <p> for the question and radio buttons for 
-    //the choices
-    var newPForQuestions = $('<p>');
-    $('#showChoices').text('');
-    for (var i = 0; i < questions[count].choices.length; i++ ) {
-       
-        $('#showChoices').append('<button value = "' + i + '" class = "buttons">' + questions[count].choices[i] + '</button><br>');
-    }
-    //TODO: after above is finished append question div to the html
-    $('#showQuestion').text(questions[count].question);
-    
-}
 
-function nextQuestion() {
-    //  TODO: Increment the count by 1.
-  if (count + 1 < questions.length) {
-    //   clearTimeout(nextQuestion);
-    count++;
-    
-    // TODO: Use a setTimeout to run displayImage after 1 second.
-    setTimeout(showQuestion(questions[count].question, questions[count].choices), 1000*5);
-  }
-}
-showQuestion(questions[count].question, questions[count].choices);
-
-//TODO: click event handler for submit button
-    // loop thru all the questions in the html using jquery
-        // loop thru all radio buttons for this question and push the value attribute of the 
-        //selected radio button to an array of choices
-
-    //TODO: compare the lenghth of the userChoices array to the length of the questions array
-    //loop thru all user choices array compare each value to the questions array
-    var answerIndex = questions[count].answerIndex;
-    $('.buttons').on('click', function() {
-        answerIndex = this.value
-       console.log(answerIndex);
-   })
-    
-    
-    if (questions[count].choices[answerIndex] === answerIndex) {
-        //TODO: thisIsACorrectAnswer
-        alert('correct');
-    } else {
-        // this is incorrect answer
-        alert('incorrect');
-    }
